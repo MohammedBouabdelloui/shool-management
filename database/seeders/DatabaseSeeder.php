886acd85a94943be96f69441c698e195f2012ffa
MOHAMMED BOUABDELLAOUI <118568976+SimoWoold@users.noreply.github.com>
2023-11-05 23:12:52 +0100

    create and migration table and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use GuzzleHttp\Promise\Create;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(6)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'mohammed',
+            'email' => 'simoboolz@gmil.com',
+            'email_verified_at' => now(),
+            'password' => 'fjkd', // password
+            'remember_token' => Str::random(10)
+        ]);
     }
 }
