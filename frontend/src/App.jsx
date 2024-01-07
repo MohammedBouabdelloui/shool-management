edb9263a5e0dddaeb634d4be303d970db2a0795f
MOHAMMED BOUABDELLAOUI <118568976+MohammedBouabdelloui@users.noreply.github.com>
2024-01-07 20:21:02 +0100

    update project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <> 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import Studentlogin from "../components/student/Studentlogin"
 
 const Login =()=>{
-return(<>
+return(
+<>
       <h1 className='flex justify-center text-3xl'>student page login</h1>
       <div className="container mx-auto ">
         <Studentlogin />
